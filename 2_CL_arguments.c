86152d177f5669c42cc7debd8ccc6e3bd0605257
AR <aarokan@gmail.com>
2024-02-05 22:25:23 +0300

    Loop through argv & print the arguments

--- 2_CL_arguments.c
+++ 2_CL_arguments.c
@@ -1,10 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, string argv[])
 {
     printf("argc : %i", argc);
-    printf("\nargv0 : %s", argv[0]);
-    printf("\nargv1 : %s", argv[1]);
+    
+    // loop through argv array and print arguments
+    for (int i = 0; i < argc; i++)
+    {
+        printf("\nargv[%i] : %s", i, argv[i]);
+    }
     printf("\n");
 }
