e037d126448fd9bc27231bbb3b1ad4f2a875b159
AR <aarokan@gmail.com>
2024-02-12 20:19:27 +0300

    print the complete sequence

--- 3_sequence.c
+++ 3_sequence.c
@@ -8,5 +8,13 @@ int main(int argc, string argv[])
     int sequence[size];
 
     sequence[0] = 1;
+    printf("%i\n", sequence[0]);
+
+    for (int i = 1; i < size; i++)
+    {
+        sequence[i] = sequence[i - 1] * 2;
+        printf("%i\n", sequence[i]);
+    }
+    printf("\n");
     
 }
